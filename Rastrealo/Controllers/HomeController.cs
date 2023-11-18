559f70bca67eeca8ec8bf9472ede0f48fb13465f
Shashank Bagda <shashankbagda167@gmail.com>
2023-11-18 14:45:21 +0530

    dotnet working files 1

---
+++ Rastrealo/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Rastrealo.Models;
+
+namespace Rastrealo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult student()
+        {
+            return View();
+        }
+
+        public IActionResult admin()
+        {
+            return View();
+        }
+
+        public IActionResult driver()
+        {
+            return View();
+        }
+
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Rastrealo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rastrealo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
