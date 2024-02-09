4ce375f704a745831a144a2b8523660974787477
Shashank Bagda <shashankbagda167@gmail.com>
2024-02-09 20:25:34 +0530

    .

--- Rastrealo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Rastrealo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
