7c33b2c61b8e9ea66a477e9b5612184769e38128
Shashank Bagda <shashankbagda167@gmail.com>
2024-03-12 11:40:00 +0530

    .

---
+++ script.js
@@ -0,0 +1,27 @@
+// script.js file
+
+function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+domReady(function () {
+
+    // If found your qr code
+    function onScanSuccess(decodeText, decodeResult) {
+        alert("Redirecting to " + decodeText);
+        window.location.href = decodeText; // Redirect to the decoded URL
+    }
+
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbos: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
+});
